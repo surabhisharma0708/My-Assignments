8eb3369498fbcd90aafbe1ab75eb2809a559a096
surabhisharma0708 <60192102+surabhisharma0708@users.noreply.github.com>
2023-04-28 23:18:55 +0530

    declaration of result based on marks in the three subject.

---
+++ Assignment1Q4.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class ResultDeclaration{
+    public String declareResults( double subject1Marks, double subject2Marks, double subject3Marks)
+    {
+        if((subject1Marks>60 || subject2Marks>60 || subject3Marks>60) || subject1Marks+subject2Marks+subject3Marks<60)
+        {
+            System.out.println("failed");
+            return "";
+        }
+        if(subject1Marks+subject2Marks+subject3Marks>60)
+        {
+            System.out.println("passed");
+        }
+        if(subject1Marks+subject2Marks>60 || subject2Marks+subject3Marks>60 || subject1Marks+subject3Marks>60)
+        {
+            System.out.println("promoted");
+        }
+
+        return "";
+    }
+}
+public class Assignment1Q4
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int one=sc.nextInt();
+        int two = sc.nextInt();
+        int three = sc.nextInt();
+        ResultDeclaration obj = new ResultDeclaration();
+        System.out.println(obj.declareResults(one, two, three));
+    }
+}
