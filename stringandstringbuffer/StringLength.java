1443cffa4e78c884741868129415616a4c5900ff
surabhisharma0708 <surabhi.sharma_cs19@gla.ac.in>
2023-05-23 13:23:44 +0530

    finding length of String

---
+++ stringandstringbuffer/StringLength.java
@@ -0,0 +1,13 @@
+package stringandstringbuffer;
+
+import java.util.Scanner;
+
+public class StringLength
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println("String length is "+s.length());
+    }
+}
