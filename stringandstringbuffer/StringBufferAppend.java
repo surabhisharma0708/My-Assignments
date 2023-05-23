f2257869e2105ab1a454ebe332dcb806610828db
surabhisharma0708 <surabhi.sharma_cs19@gla.ac.in>
2023-05-23 13:51:21 +0530

    performing append operation of StringBuffer

---
+++ stringandstringbuffer/StringBufferAppend.java
@@ -0,0 +1,14 @@
+package stringandstringbuffer;
+
+public class StringBufferAppend
+{
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("StrigBuffer");
+        sb.append(" is a peer class of String ");
+        sb.append("that provides much of ");
+        sb.append("the functionality of strings");
+
+        System.out.println(sb);
+    }
+}
